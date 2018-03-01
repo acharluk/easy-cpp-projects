3455db339ca9354c6b7182c20da856fb6d7c644e
ACharLuk <acharluk@gmail.com>
2018-03-01 17:22:18 +0100

    Open main.cpp

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,41 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { mkdirSync, writeFileSync } from 'fs';
+import fetch from 'node-fetch';
+
+const baseUrl = 'https://raw.githubusercontent.com/acharluk/easy-cpp-projects/master';
+
+interface CppProject {
+    directories: string[];
+    download: string[];
+}
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "easy-cpp-projects" is now active!');
 
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let createProjectCommand = vscode.commands.registerCommand('easycpp.createProject', () => {
+        fetch(baseUrl + '/cpp/files.json')
+        .then(res => res.json())
+        .then((files: CppProject) => {
+            files.directories.forEach((dir: string) => {
+                mkdirSync(vscode.workspace.rootPath + '/' + dir);
+            });
 
-        vscode.window.showInformationMessage('Hello World!');
+            files.download.forEach((file: string) => {
+                fetch(baseUrl + '/cpp/template/' + file)
+                .then(res => res.text())
+                .then(data => {
+                    writeFileSync(vscode.workspace.rootPath + '/' + file, data);
+                    if (file === 'src/main.cpp') {
+                        vscode.workspace.openTextDocument(vscode.workspace.rootPath + '/src/main.cpp')
+                        .then(doc => vscode.window.showTextDocument(doc));
+                    }
+                });
+            });
+        });
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(createProjectCommand);
 }
 
 export function deactivate() {
