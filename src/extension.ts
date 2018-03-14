68f4fa580bd114242a9b017be4660dc3bf9600c2
ACharLuk <acharluk@gmail.com>
2018-03-14 18:49:19 +0100

    Add multiple templates support

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,56 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { mkdirSync, writeFileSync } from 'fs';
+import { mkdirSync, writeFileSync, existsSync } from 'fs';
 import fetch from 'node-fetch';
 
 const baseUrl = 'https://raw.githubusercontent.com/acharluk/easy-cpp-projects/master';
 
-interface CppProject {
-    directories: string[];
-    download: string[];
-}
-
 export function activate(context: vscode.ExtensionContext) {
 
     let createProjectCommand = vscode.commands.registerCommand('easycpp.createProject', () => {
-        fetch(baseUrl + '/cpp/files.json')
-        .then(res => res.json())
-        .then((files: CppProject) => {
-            files.directories.forEach((dir: string) => {
-                mkdirSync(vscode.workspace.rootPath + '/' + dir);
-            });
+        fetch(baseUrl + '/templates/files.json')
+            .then(res => res.json())
+            .then(data => {
+                let templates = [];
+                for (let tname in data.templates) { templates.push(tname); }
 
-            files.download.forEach((file: string) => {
-                fetch(baseUrl + '/cpp/template/' + file)
-                .then(res => res.text())
-                .then(data => {
-                    writeFileSync(vscode.workspace.rootPath + '/' + file, data);
-                    if (file === 'src/main.cpp') {
-                        vscode.workspace.openTextDocument(vscode.workspace.rootPath + '/src/main.cpp')
-                        .then(doc => vscode.window.showTextDocument(doc));
-                    }
-                })
-                .catch(error => console.error("Easy C++ Projects error: " + error));
-            });
-        })
-        .catch(error => console.error("Easy C++ Projects error: " + error));
+                vscode.window.showQuickPick(templates)
+                .then(selected => downloadTemplate(selected));
+            })
+            .catch(error => console.error("Easy C++ Projects error: " + error));
     });
 
     context.subscriptions.push(createProjectCommand);
 }
 
 export function deactivate() {
+}
+
+function downloadTemplate(templateName: string | undefined): void {
+    if (!templateName) { return; }
+
+    fetch(baseUrl + '/templates/files.json')
+        .then(res => res.json())
+        .then(files => {
+            files.directories.forEach((dir: string) => {
+                if (!existsSync(vscode.workspace.rootPath + '/' + dir)) {
+                    mkdirSync(vscode.workspace.rootPath + '/' + dir);
+                }
+            });
+
+            for (let file in files.templates[templateName]) {
+                fetch(baseUrl + '/templates/' + "/" + file)
+                .then(res => res.text())
+                .then(data => {
+                    writeFileSync(vscode.workspace.rootPath + '/' + files.templates[templateName][file], data);
+                    if (files.templates[templateName][file] === 'src/main.cpp') {
+                        vscode.workspace.openTextDocument(vscode.workspace.rootPath + '/src/main.cpp')
+                        .then(doc => vscode.window.showTextDocument(doc));
+                    }
+                })
+                .catch(error => console.error("Easy C++ Projects error: " + error));
+            }
+        })
+        .catch(error => console.error("Easy C++ Projects error: " + error));
 }
