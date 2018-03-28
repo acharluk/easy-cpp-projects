723bfc618a916402374e7b6a09f35273da1f02a9
ACharLuk <acharluk@gmail.com>
2018-03-28 20:52:25 +0200

    Removed fetching 'files.json' two times

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,12 @@ import fetch from 'node-fetch';
 
 const baseUrl = 'https://raw.githubusercontent.com/acharluk/easy-cpp-projects/master';
 
-export function activate(context: vscode.ExtensionContext) {
+interface EasyFilesJSON {
+    directories: string[];
+    templates: { [name: string]: { [from: string]: string } };
+}
 
+export function activate(context: vscode.ExtensionContext) {
     let createProjectCommand = vscode.commands.registerCommand('easycpp.createProject', () => {
         fetch(baseUrl + '/templates/files.json')
             .then(res => res.json())
@@ -16,9 +20,9 @@ export function activate(context: vscode.ExtensionContext) {
                 for (let tname in data.templates) { templates.push(tname); }
 
                 vscode.window.showQuickPick(templates)
-                .then(selected => downloadTemplate(selected));
+                .then(selected => downloadTemplate(data, selected));
             })
-            .catch(error => console.error("Easy C++ Projects error: " + error));
+            .catch(error => console.error("Easy C++ Projects error: Could not fetch 'files.json' from GitHub\nError: " + error));
     });
 
     context.subscriptions.push(createProjectCommand);
@@ -27,30 +31,25 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
-function downloadTemplate(templateName: string | undefined): void {
+function downloadTemplate(files: EasyFilesJSON, templateName: string | undefined): void {
     if (!templateName) { return; }
 
-    fetch(baseUrl + '/templates/files.json')
-        .then(res => res.json())
-        .then(files => {
-            files.directories.forEach((dir: string) => {
-                if (!existsSync(vscode.workspace.rootPath + '/' + dir)) {
-                    mkdirSync(vscode.workspace.rootPath + '/' + dir);
-                }
-            });
+    files.directories.forEach((dir: string) => {
+        if (!existsSync(vscode.workspace.rootPath + '/' + dir)) {
+            mkdirSync(vscode.workspace.rootPath + '/' + dir);
+        }
+    });
 
-            for (let file in files.templates[templateName]) {
-                fetch(baseUrl + '/templates/' + "/" + file)
-                .then(res => res.text())
-                .then(data => {
-                    writeFileSync(vscode.workspace.rootPath + '/' + files.templates[templateName][file], data);
-                    if (files.templates[templateName][file] === 'src/main.cpp') {
-                        vscode.workspace.openTextDocument(vscode.workspace.rootPath + '/src/main.cpp')
-                        .then(doc => vscode.window.showTextDocument(doc));
-                    }
-                })
-                .catch(error => console.error("Easy C++ Projects error: " + error));
+    for (let file in files.templates[templateName]) {
+        fetch(baseUrl + '/templates/' + "/" + file)
+        .then(res => res.text())
+        .then(data => {
+            writeFileSync(vscode.workspace.rootPath + '/' + files.templates[templateName][file], data);
+            if (files.templates[templateName][file] === 'src/main.cpp') {
+                vscode.workspace.openTextDocument(vscode.workspace.rootPath + '/src/main.cpp')
+                .then(doc => vscode.window.showTextDocument(doc));
             }
         })
-        .catch(error => console.error("Easy C++ Projects error: " + error));
+        .catch(error => console.error(`Easy C++ Projects error: Could not download '${file}' from GitHub\nError: ` + error));
+    }
 }
