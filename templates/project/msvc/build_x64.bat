43aa3768b1bdb2117a8e7c9700c9a9693a5e226f
ACharLuk <acharluk@gmail.com>
2018-06-04 13:46:27 +0200

    Add include flag to msvc scripts

--- templates/project/msvc/build_x64.bat
+++ templates/project/msvc/build_x64.bat
@@ -1,6 +1,6 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-set compilerflags=/Od /Zi /EHsc /std:c++latest
+set compilerflags=/Od /Zi /EHsc /std:c++latest /I include
 set linkerflags=/OUT:bin\main.exe
 cl.exe %compilerflags% src\*.cpp /link %linkerflags%
 del bin\*.ilk *.obj *.pdb bin\*.pdb
