1bb732e31134542737145fd25abaf2fecdeae98c
Meister182 <c137.marques@gmail.com>
2019-07-24 23:26:40 +0100

    [#17] updating build.bat to remove temporary files

--- templates/project/msvc/build_x86.bat
+++ templates/project/msvc/build_x86.bat
@@ -7,4 +7,4 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxi
 set compilerflags=/Od /Zi /EHsc /std:c++latest /I include
 set linkerflags=/OUT:bin\main.exe
 cl.exe %compilerflags% src\*.cpp /link %linkerflags%
-del bin\*.ilk *.obj *.pdb
+del bin\*.ilk bin\*.pdb *.obj *.pdb
